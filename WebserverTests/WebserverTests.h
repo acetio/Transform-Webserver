d039ec43c2c9269c2ac0dfcb2ad8c12841c30f47
Nicholas Jackson <nic@thatlondon.com>
2011-05-07 21:17:25 +0100

    initial commit

---
+++ WebserverTests/WebserverTests.h
@@ -0,0 +1,17 @@
+//
+//  WebserverTests.h
+//  WebserverTests
+//
+//  Created by Nic Jackson on 09/04/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface WebserverTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ WebserverTests/WebserverTests.m
@@ -0,0 +1,33 @@
+//
+//  WebserverTests.m
+//  WebserverTests
+//
+//  Created by Nic Jackson on 09/04/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "WebserverTests.h"
+
+
+@implementation WebserverTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in WebserverTests");
+}
+
+@end
